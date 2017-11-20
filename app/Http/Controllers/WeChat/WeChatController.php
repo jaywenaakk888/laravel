0ce59dd96871227015da9abc939939ef610541ce
wen <455744794@qq.com>
2017-11-20 22:51:44 +0800

    修改微信

--- app/Http/Controllers/WeChat/WeChatController.php
+++ app/Http/Controllers/WeChat/WeChatController.php
@@ -18,11 +18,12 @@ class WeChatController extends Controller
     {
         Log::info('request arrived.'); # 注意：Log 为 Laravel 组件，所以它记的日志去 Laravel 日志看，而不是 EasyWeChat 日志
         
-        $app = app('wechat');
-        $app->server->push(function($message){
+        $wechat = app('wechat');
+        $wechat->server->setMessageHandler(function($message){
             return "欢迎关注 overtrue！";
         });
-
-        return $app->server->serve();
+        Log::info('return response.');
+        
+        return $wechat->server->serve();
     }
 }
